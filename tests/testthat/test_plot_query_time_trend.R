a67bb8db13693e9c069a17714e40a66665d92b1f
yukooD <58755699+Yukodeng@users.noreply.github.com>
2023-12-20 13:59:43 -0500

    last commit

--- tests/testthat/test_plot_query_time_trend.R
+++ tests/testthat/test_plot_query_time_trend.R
@@ -2,7 +2,7 @@ test_that("plot_qeury_time_trend() works", {
   data(crc_sub)
   vdiffr::expect_doppelganger(
     "plot_drug_time_trend",
-    plot_query_time_trend('drug', quo(intervention_type))
+    plot_query_time_trend('drug', dplyr::quo(intervention_type))
   )
 })
 
